3902091cdcc440bb0d49d832be4c45b629517aa9
jpeart <jpeart4@gmail.com>
2017-07-25 19:56:57 -0500

    bleh

---
+++ liri.js
@@ -0,0 +1,5 @@
+var keys = require("./keys.js");
+//  consumer_key, consumer_secret, access_token_key, access_token_secret
+keys = keys.twitterKeys;
+
+// commands to recieve: `my-tweets` `spotify-this-song` `movie-this` `do-what-it-says'
